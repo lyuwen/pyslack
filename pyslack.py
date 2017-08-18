698139e778fd4a388c6c173397ff9cb0950d13be
Lyuwen <lyuwen@users.noreply.github.com>
2017-08-18 14:59:51 -0400

    Update README.md.  new user post message feature. different from bot post ,added usage text in the code. incoming webhook is optional now.

--- pyslack.py
+++ pyslack.py
@@ -4,14 +4,29 @@ import requests
 import configparser
 from inputs import inputs
 CONFIG=os.path.join(os.environ['HOME'],'.pyslackrc')
-''' This is almost same as vim-slack setting
-$ cat .pyslackrc
-[Slack]
-slack_fileupload_token=TOKEN1
-slack_incoming_token=TOKEN2
+'''
+Command line usage:
+  pyslack.py
+    file mode
+    -file            file name 
+    -channel         channel name with # or direct message with @
+    -title           the title of the post
+    -initial_comment the initial comment to be added to the post
+
+    post mode
+    -text            post message
+    -channel         channel name with # or direct message with @
+
+.pyslackrc file
+  $ cat .pyslackrc
+  [Slack]
+  slack_fileupload_token=TOKEN1  #Required
+  slack_incoming_token=TOKEN2    #
 '''
 
 class pySlack(object):
+  ''' A python interface to slack
+  '''
   def __init__(self):
     self.config = configparser.ConfigParser()
     self.config.read(CONFIG)
@@ -20,11 +35,12 @@ class pySlack(object):
     if dir(self).count('slack_fileupload_token')==0:
       print 'Missing slack_fileupload_token'
       sys.exit()
-    if dir(self).count('slack_incoming_token')==0:
-      print 'Missing slack_incoming_token'
-      sys.exit()
+    #if dir(self).count('slack_incoming_token')==0:
+    #  print 'Missing slack_incoming_token'
+    #  sys.exit()
 
   def get_channel_ids(self):
+    if not (hasattr(self,"slack_fileupload_token") and self.slack_fileupload_token!=''): raise Exception("slack_fileupload_token not available. Cannot get channel ids!")
     headers = {'content-type': 'application/x-www-form-urlencoded'}
     respond=requests.post("https://slack.com/api/channels.list",headers=headers, data={u'token':self.slack_fileupload_token})
     all_channels=dict(('#'+s['name'],s['id']) for s in respond.json()['channels'] if s["is_member"]==True)
@@ -34,11 +50,12 @@ class pySlack(object):
     self.channels=all_channels
 
 
-  def post(self, **kwargs):
+  def bot_post(self, **kwargs):
     '''
       Available flags
       text, username, channel, icon_emoji
     '''
+
     payload={
         'text': '',
         'channel': '#general',
@@ -46,9 +63,35 @@ class pySlack(object):
         'icon_emoji': ':ghost:',
         }
     payload.update(kwargs)
+    if not (hasattr(self,"slack_incoming_token") and self.slack_incoming_token!=''): raise Exception("Slack_incoming_token not available. Cannot post as bot")
     headers = {'content-type': 'application/json'}
     respond=requests.post(self.slack_incoming_token,headers=headers, data=json.dumps(payload))
     print respond.text
+
+  def post(self, **kwargs):
+    '''
+      Available flags
+      token, text, channel, as_user, username, icon_emoji
+    '''
+    payload={
+        #'channels': '#general',
+        'as_user': True,
+        }
+    payload.update(kwargs)
+
+    url='https://slack.com/api/chat.postMessage'
+    if payload.keys().count('text')==0:raise Exception("Missing text!")
+    if not (hasattr(self,"slack_fileupload_token") and self.slack_fileupload_token!=''): raise Exception("slack_fileupload_token not available. Cannot post!")
+    payload['token']=self.slack_fileupload_token
+    #options=[]
+    #for s in payload.keys():
+    #  options.append('-F %s="%s"'%(s,payload[s]))
+    headers = {'content-type': 'application/x-www-form-urlencoded'}
+    respond=requests.post(url,headers=headers, data=payload)
+    #print cmd
+    if respond.json()['ok']:print "Done"
+    else: raise Exception("Posting error...")
+
 
   def uploadFile(self, **kwargs):
     '''
@@ -69,6 +112,7 @@ class pySlack(object):
     if payload.keys().count('file'):
       if payload.keys().count('filename')==0:payload['filename']=payload['file']
       if payload.keys().count('title')==0:payload['title']=payload['file']
+    if not (hasattr(self,"slack_fileupload_token") and self.slack_fileupload_token!=''): raise Exception("slack_fileupload_token not available. Cannot post!")
     payload['token']=self.slack_fileupload_token
     options=[]
     for s in payload.keys():
@@ -93,9 +137,9 @@ if __name__=="__main__":
   pyslack.get_channel_ids()
   #pyslack.post(text='Hello world!')
   if av.count("channel"):
+    channel_id=pyslack.channels[av.channel]
     if av.count('file'):
       print "file mode"
-      channel_id=pyslack.channels[av.channel]
       if os.path.exists(av.file):
         if av.count('I'):
           # Interactively gather information
@@ -124,7 +168,7 @@ if __name__=="__main__":
       print "posting mode"
       if av.count('y') or raw_input( "Post \"{}\" to channel {}, proceed?[yN]: ".format(av.text,av.channel)).lower() in ['y','yes']: 
         print "posting"
-        pyslack.post(**av.get_dict())
+        pyslack.post(channel=channel_id,**dict((s,ss) for s,ss in av.get_dict().items() if s!="channel"))
       else:
         print "Abort"
     else:
